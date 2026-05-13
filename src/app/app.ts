38b55ea5f687463ef077982d9bacb7f7f04792e0
renzodrummer <renzo071598@gmail.com>
2026-05-13 21:49:08 +0800

    first commit

---
+++ src/app/app.ts
@@ -0,0 +1,15 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  protected readonly title = signal('vmg-sentinel');
+  
+  constructor(){
+    console.log("Hello")
+  }
+}
